61d903cb6ab2846e0e00ba52378d6ccd4a2cc490
Salma Aulia Nazhira <127862908+SalmaAuliaNazhira@users.noreply.github.com>
2023-05-14 21:51:35 +0700

    NotImplementedError.py

--- tugas5/NotImplementedError.py
+++ tugas5/NotImplementedError.py
@@ -7,3 +7,17 @@ try:
     print("So cool you earn %d amount." % salary)
 except NotImplementedError:
     print("Hey, that wasn't a number!")
+
+# another example
+class BaseClass:
+    def some_method(self):
+        raise NotImplementedError("This method is not implemented")
+
+class DerivedClass(BaseClass):
+    pass
+
+try:
+    obj = DerivedClass()
+    obj.some_method()
+except NotImplementedError as e:
+    print("NotImplementedError:", e)
