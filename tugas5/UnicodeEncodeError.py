226211da816c7f110036e4218364750c4a74ca38
Salma Aulia Nazhira <127862908+SalmaAuliaNazhira@users.noreply.github.com>
2023-05-14 21:55:04 +0700

    Delete UnicodeEncodeError.py

--- tugas5/UnicodeEncodeError.py
+++
@@ -1,9 +0,0 @@
-# Nama    : Salma Aulia Nazhira
-# NIM     : 210511132
-# Kelas   : R3/TI21C
-
-a = u'café'
-b = a.encode('utf8')
-r = str(b)
-print("The unicode string after fixing the UnicodeEncodeError is as follows:")
-print(r)
